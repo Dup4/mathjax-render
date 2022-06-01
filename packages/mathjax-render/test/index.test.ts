3e4df7eb01737537aea0246d2703b591655c868f
Dup4 <lyuzhi.pan@gmail.com>
2022-06-01 15:33:13 +0800

    feat: Tex2SVG support node

--- packages/mathjax-render/test/index.test.ts
+++ packages/mathjax-render/test/index.test.ts
@@ -1,8 +1,18 @@
+import glob from "glob";
+import path from "path";
+import fs from "fs";
 import { describe, expect, it } from "vitest";
-import { add2 } from "../src/index";
+import { Tex2SVG } from "../src/index";
 
-describe("should", () => {
-  it("add2", () => {
-    expect(add2(1, 2)).toEqual(3);
-  });
+// https://www.svgviewer.dev/
+
+describe("Tex2SVG", () => {
+  for (const target of glob.sync(path.join(__dirname, "./test-data/*.tex"))) {
+    const basename = path.basename(target, ".tex");
+    it(basename, () => {
+      const content = fs.readFileSync(target).toString();
+      const result = Tex2SVG(content, true);
+      expect(result).toMatchSnapshot();
+    });
+  }
 });
