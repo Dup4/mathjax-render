ae0108d0bfa6a60c86fcb4b0f012a2eb2de046c6
Dup4 <lyuzhi.pan@gmail.com>
2022-06-01 19:49:54 +0800

    chore: rename .html -> .data

--- packages/mathjax-render-for-mkdocs-material/test/RenderMathJax.test.ts
+++ packages/mathjax-render-for-mkdocs-material/test/RenderMathJax.test.ts
@@ -5,8 +5,8 @@ import { describe, expect, it } from "vitest";
 import { RenderMathJax } from "../src/index";
 
 describe("RenderMathJax", () => {
-  for (const target of glob.sync(path.join(__dirname, "./test-data/*.html"))) {
-    const basename = path.basename(target, ".html");
+  for (const target of glob.sync(path.join(__dirname, "./test-data/*.data"))) {
+    const basename = path.basename(target, ".data");
     it(basename, () => {
       const content = fs.readFileSync(target).toString();
       const result = RenderMathJax(content);
