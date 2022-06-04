8c7a4ef6f105c224142840c27c8f9ab63c138703
Dup4 <lyuzhi.pan@gmail.com>
2022-06-04 12:09:48 +0800

    feat: impl simple MathJaxNode

---
+++ packages/mathjax-render-react/src/MathJaxNode.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Options, Tex2SVG } from "mathjax-render";
+
+export interface MathJaxNodeProps extends Options {
+  tex: string;
+  display: boolean;
+}
+
+export const MathJaxNode: React.FC<MathJaxNodeProps> = (props) => {
+  const { tex, display, ...options } = props;
+
+  if (display) {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: Tex2SVG(tex, display, options).outerHTML,
+        }}
+      ></div>
+    );
+  }
+
+  return (
+    <span
+      dangerouslySetInnerHTML={{
+        __html: Tex2SVG(tex, display, options).outerHTML,
+      }}
+    ></span>
+  );
+};
