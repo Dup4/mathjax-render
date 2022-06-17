4b9e10c9bbb5443f0a4cc85da4143db393fac09a
Dup4 <lyuzhi.pan@gmail.com>
2022-06-17 10:07:21 +0800

    chore: add dark mode

--- examples/mathjax-render-react-example/src/pages/_app.tsx
+++ examples/mathjax-render-react-example/src/pages/_app.tsx
@@ -1,17 +1,41 @@
 import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 import NProgress from "@/components/nprogress";
 
 import "@/styles/globals.css";
 import "@/styles/nprogress.css";
 
+const lightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {},
+  },
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {},
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <NProgress />
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme={lightTheme.className}
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <NProgress />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
 
