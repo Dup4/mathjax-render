d0353d1ac5349d8b92d0b9e75278603a0a82820a
Dup4 <lyuzhi.pan@gmail.com>
2023-07-08 23:18:59 +0800

    test: fix path alias
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- alias.ts
+++ alias.ts
@@ -3,9 +3,10 @@ import { resolve } from "path";
 const r = (p: string) => resolve(__dirname, p);
 
 export const alias: Record<string, string> = {
-  "mathjax-render": r("./packages/mathjax-render/src/node/"),
+  "mathjax-render": r("./packages/libs/mathjax-render/src/node/"),
   "mathjax-render-for-mkdocs-material": r(
-    "./packages/mathjax-render-for-mkdocs-material/src",
+    "./packages/libs/mathjax-render-for-mkdocs-material/src",
   ),
-  "mathjax-render-react": r("./packages/mathjax-render-react/src/"),
+  "mathjax-render-react": r("./packages/libs/mathjax-render-react/src/"),
+  "mathjax-render-nextjs": r("./packages/libs/mathjax-render-nextjs/src"),
 };
